991dd7a8bd5704f781c23f36c73b0482eb22d9ae
josiels <jofe2006@yahoo.com.br>
2023-04-02 22:50:17 -0300

    Atualizar o index.php

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Index</title>
 </head>
 <body>
-    INDEX 1
+    <h1>Index</h1>
+    <h3>Pasta <strong>projetobase/public/</strong></h3>
 </body>
 </html>
